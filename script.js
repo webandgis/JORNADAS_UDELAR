904b6583feb385b16f7e046ef9c66d444990dc7d
girmar14 <girmarcity@hotmail.com>
2025-07-08 18:01:58 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@
 
   // Capa base debajo del swipe
 
-  const baselayer= L.tileLayer('https://tiles.stadiamaps.com/tiles/alidade_satellite/{z}/{x}/{y}{r}.{ext}', {
+  const baselayer= L.tileLayer('https://server.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}', {
     minZoom: 0,
     maxZoom: 20,
     attribution: '&copy; CNES, Distribution Airbus DS, © Airbus DS, © PlanetObserver (Contains Copernicus Data) | &copy; <a href="https://www.stadiamaps.com/" target="_blank">Stadia Maps</a> &copy; <a href="https://openmaptiles.org/" target="_blank">OpenMapTiles</a> &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
